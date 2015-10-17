7b4488934b62a007ddccd93ecd3d2c389610e51a
Steve Kondik <steve@cyngn.com>
2015-10-16 21:29:05 -0700

    bt-caf: Add guard makefile
    Change-Id: I89be4bd031c36ce698b7531e9fc66b53c9c13aec

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(call my-dir),$(call project-path-for,bt-vendor))
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -13,3 +15,5 @@ include $(TMP_LOCAL_PATH)/tools/Android.mk
 ifeq ($(TARGET_USE_QTI_BT_STACK),true)
 include $(TMP_LOCAL_PATH)/bthost_ipc/Android.mk
 endif #TARGET_USE_QTI_BT_STACK
+
+endif
